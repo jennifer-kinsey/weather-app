a718a19ee124981a822ae27b62f3bac067c2e226
Jennifer Kinsey and Monique St Laurent <jennifer@>
2017-05-23 11:57:58 -0700

    adds the temperature and description for weather.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,24 @@
-// var Weather = require('./../js/weather.js').weatherModule;
-var apiKey = "f13f28aae766b371c88539e6329823cd";
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayDescription = function(city, descriptionData) {
+  $('.showDescription').text("You can expect " + descriptionData + " today in " + city);
+}
+
+var displayTemperatures = function(city, currentTemp) {
+  $('.showTemperatures').text("The current temperature is " + currentTemp + "&#176; &deg;  .");
+}
+
+var displayHumidity = function(city, humidityData) {
+  $('.showHumidity').text("The humidity is " + humidityData + "%.");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weather-location').click(function() {
-    var city = $('#city').val();
-    $('#city').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getHumidity(city, displayHumidity);
+    currentWeatherObject.getDescription(city, displayDescription);
+    currentWeatherObject.getTemperatures(city, displayTemperatures);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,37 @@
-// function Weather(city) {
-//   this.city = city;
-// }
-//
-// exports.weatherModule = Weather;
+var apiKey = require('./../.env').apiKey;
+
+Weather = function(){
+}
+
+Weather.prototype.getHumidity = function(city, displayHumidity) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displayHumidity(city, response.main.humidity);
+  }).fail(function(error) {
+    $('.showHumidity').text(error.responseJSON.message);
+  });
+}
+
+Weather.prototype.getDescription = function(city, displayDescription) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displayDescription(city, response.weather[0].description);
+  }).fail(function(error) {
+    $('.showDescription').text(error.responseJSON.message);
+  });
+}
+
+Weather.prototype.getTemperatures = function(city, displayTemperatures) {
+  convertToFahr = function (kTemp) {
+    fahr = 1.8 * (kTemp - 273) + 32;
+    return fahr;
+  };
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displayTemperatures(city, convertToFahr(response.main.temp));
+  }).fail(function(error) {
+    $('.showTemperatures').text(error.responseJSON.message);
+  });
+}
+
+
+
+exports.weatherModule = Weather;
