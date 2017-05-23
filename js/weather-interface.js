513ff620b97b15b0f1b3ba6bdb313d3b2f411481
Jennifer Kinsey and Monique St Laurent <monique@>
2017-05-23 14:51:08 -0700

    adds sunrise and sunset times converted from epoch time.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,11 +5,19 @@ var displayDescription = function(city, descriptionData) {
 }
 
 var displayTemperatures = function(city, currentTemp) {
-  $('.showTemperatures').text("The current temperature is " + currentTemp + "&#176; &deg;  .");
+  $('.showTemperatures').text("The current temperature is " + currentTemp + "\u2109.");
 }
 
 var displayHumidity = function(city, humidityData) {
   $('.showHumidity').text("The humidity is " + humidityData + "%.");
+}
+
+var displaySunrise = function(city, sunriseData) {
+  $('.showSunrise').text("The sun rises at " + sunriseData + " PST.")
+}
+
+var displaySunset = function(city, sunsetData) {
+  $('.showSunset').text("The sun sets at " + sunsetData + " PST.")
 }
 
 $(document).ready(function() {
@@ -20,5 +28,7 @@ $(document).ready(function() {
     currentWeatherObject.getHumidity(city, displayHumidity);
     currentWeatherObject.getDescription(city, displayDescription);
     currentWeatherObject.getTemperatures(city, displayTemperatures);
+    currentWeatherObject.getSunrise(city, displaySunrise);
+    currentWeatherObject.getSunset(city, displaySunset);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -22,9 +22,8 @@ Weather.prototype.getDescription = function(city, displayDescription) {
 Weather.prototype.getTemperatures = function(city, displayTemperatures) {
   convertToFahr = function (kTemp) {
     fahr = 1.8 * (kTemp - 273) + 32;
-    return fahr;
+    return Math.round(fahr);
   };
-
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
     displayTemperatures(city, convertToFahr(response.main.temp));
   }).fail(function(error) {
@@ -32,6 +31,28 @@ Weather.prototype.getTemperatures = function(city, displayTemperatures) {
   });
 }
 
+Weather.prototype.getSunrise = function(city, displaySunrise) {
+  converttoSunrise = function(epochTime) {
+    var myDate = new Date(epochTime *1000);
+    return myDate.toLocaleString();
+  };
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displaySunrise(city, converttoSunrise(response.sys.sunrise));
+  }).fail(function(error) {
+    $('.showSunrise').text(error.responseJSON.message);
+  });
+}
 
+Weather.prototype.getSunset = function(city, displaySunset) {
+  converttoSunset = function(epochTime) {
+    var myDate = new Date(epochTime *1000);
+    return myDate.toLocaleString();
+  };
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displaySunset(city, converttoSunset(response.sys.sunset));
+  }).fail(function(error) {
+    $('.showSunset').text(error.responseJSON.message);
+  });
+}
 
 exports.weatherModule = Weather;
